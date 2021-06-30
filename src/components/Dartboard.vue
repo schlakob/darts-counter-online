9f5f6ab92fc13488c2bce129d1acd27fa67ef242
liltschou <38752135+liltschou@users.noreply.github.com>
2021-06-30 22:43:38 +0200

    erste Spielereien

---
+++ src/components/Dartboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Hi</h1>
+</template>
+
+<script>
+export default {
+  name: 'Dartboard',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
---
+++ src/views/Darts-Counter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Dartboard msg="Turn up"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Dartboard from '@/components/Dartboard.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Dartboard
+  }
+}
+</script>
